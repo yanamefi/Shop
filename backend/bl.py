bc24a37bb7a154ced6ae498cdde502735d47a6bd
Maksimilian16 <buskomaksym08@gmail.com>
2023-04-04 09:04:41 +0300

    beta2.1

---
+++ backend/bl.py
@@ -0,0 +1,12 @@
+from settings import *
+import psycopg2
+from models import Products
+async def home_page():
+    cur.execute("""SELECT * FROM products""")
+    data = cur.fetchall()
+    formatted_photos = []
+    for prods in data:
+        formatted_photos.append(
+            Products(id=prods[0], post_date=prods[1], pr_name=prods[2], description=prods[3], price=prods[4],
+                     photo=prods[5], photo_name=prods[6]))
+    conn.commit()
